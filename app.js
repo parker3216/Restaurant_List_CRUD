ab54292440280b1e51b4eaf41a2e27c1ec601853
parker3216 <parker3216@hotmail.com>
2022-08-09 10:23:22 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const app = express()
+const exphbs = require('express-handlebars')//載入handlebars
+
+
 const port = 3000
+const app = express()
+
 
 const mongoose = require('mongoose') //載入 mongoose
 mongoose.connect(process.env.MONGODB_URI)//設定連線到 mongoDB
@@ -14,10 +18,14 @@ db.once('open', () =>{
   console.log('mongodb connected!')
 })
 
+app.engine('handlebars',exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
 
 
 app.get('/', (req,res) =>{
-  res.send('fuckyou')
+  res.render('index')
 })
 
 app.listen(port, () =>{
